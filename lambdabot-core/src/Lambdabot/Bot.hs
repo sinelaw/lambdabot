ad2c70ab3f5560e27a14c5cedaf9fa662f27bb63
James Cook <mokus@deepbondi.net>
2013-07-19 14:05:26 -0700

    lambdabotMain now takes a list of modules
    (instead of an LB action that loads them)

--- lambdabot-core/src/Lambdabot/Bot.hs
+++ lambdabot-core/src/Lambdabot/Bot.hs
@@ -43,8 +43,8 @@ import qualified Data.Set as S
 --
 -- | Register a module in the irc state
 --
-ircLoadModule :: Module st -> String -> LB ()
-ircLoadModule m mName = do
+ircLoadModule :: String -> Module st -> LB ()
+ircLoadModule mName m = do
     infoM ("Loading module " ++ show mName)
     
     savedState <- readGlobalState m mName
@@ -76,7 +76,7 @@ ircUnloadModule mName = do
             `E.catch` \e@SomeException{} -> 
                 errorM ("Module " ++ show mName ++ " threw the following exception in moduleExit: " ++ show e)
         
-        writeGlobalState m mName
+        writeGlobalState
     
     unregisterModule mName
 
